d10c40cdeb96b5cb06d10365fd028096c0ad8611
KodFun <kodluyorumtv@gmail.com>
2023-12-30 20:59:46 +0300

    snow flakes

---
+++ ObjectFor/index.js
@@ -0,0 +1,14 @@
+
+const p = {
+  name: "Ronaldo",
+  age: 38,
+  born: 1985,
+  height: 1.87,
+  salary: "26.8M"
+};
+
+console.log(p.name);
+console.log(p.age);
+console.log(p.born);
+console.log(p.salary);
+console.log(p.height);

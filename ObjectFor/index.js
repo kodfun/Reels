d8d6ce9784191e0d282131ebbc9633a30520b974
KodFun <kodluyorumtv@gmail.com>
2024-01-18 21:06:51 +0300

    for...in stringify

--- ObjectFor/index.js
+++ ObjectFor/index.js
@@ -1,14 +1,12 @@
-
-const p = {
-  name: "Ronaldo",
+const oyuncu = {
+  name: "Cristiano Ronaldo",
   age: 38,
   born: 1985,
-  height: 1.87,
-  salary: "26.8M"
+  height: 1.87
 };
 
-console.log(p.name);
-console.log(p.age);
-console.log(p.born);
-console.log(p.salary);
-console.log(p.height);
+for (const key in oyuncu) {
+  console.log(key, oyuncu[key]);
+}
+
+console.log(JSON.stringify(oyuncu));

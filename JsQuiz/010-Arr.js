165753a9a6fa24e81976a995ebb9ac38586f9617
KodFun <kodluyorumtv@gmail.com>
2024-05-14 19:26:43 +0300

    str reverse

---
+++ JsQuiz/010-Arr.js
@@ -0,0 +1,5 @@
+let arr = [1, 2, 3];
+arr = arr.map(x => x ** 2);
+console.log(arr.toReversed());
+
+// [ 9, 4, 1 ]
---
+++ StringReverse/index.js
@@ -0,0 +1,8 @@
+let txt = "hello";
+
+txt.split('').reverse().join('')
+
+console.log(txt);
+console.log(txt.split(''));
+console.log(txt.split('').reverse());
+console.log(txt.split('').reverse().join(''));

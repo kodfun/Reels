d9269592eb765f67c0758f3f249fbe4d4a60c720
KodFun <kodluyorumtv@gmail.com>
2024-06-27 17:59:17 +0300

    charcode

---
+++ CharCode/index.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+
+
+// SAYISAL KODUNDAN => KARAKTERE
+console.log(String.fromCharCode(65))
+// KARAKTERDEN => SAYISAL KODUNA
+console.log("A".charCodeAt(0))
+// SAYILARLA STRING TANIMLAMA
+console.log(String.fromCharCode(66,65,66,65))
